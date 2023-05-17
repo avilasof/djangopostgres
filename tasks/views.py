1dc667a5906a917680dba77bb1e7a74fefc7cd38
Ramon Bastardo <bastardoramon@gmail.com>
2023-05-16 20:47:06 -0400

    second commit: Delete Task, List Tasks and styles

--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,15 @@ from .models import Task
 
 # Create your views here.
 def list_tasks(request):
-    return render(request, 'list_tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'list_tasks.html', {"tasks": tasks})
 
 def create_task(request):
     task = Task(title=request.POST['title'], description=request.POST['description'])
     task.save()
+    return redirect('/tasks/')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
     return redirect('/tasks/')
